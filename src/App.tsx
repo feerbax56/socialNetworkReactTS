07aa93832e9ab88a49446093e5451ec5fd0ca37f
Evseev Anton <soki1997@mail.ru>
2022-08-07 21:34:36 +0500

    add: fix bags

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
                       <Route path='/setting'> <Profile /></Route>
 
               </div>
+              <div>проверка связи</div>
           </div>
       </BrowserRouter>
   );
