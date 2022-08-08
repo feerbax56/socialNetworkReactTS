fa9f0b83805549e67acd1e1cf4b5bcf600b67367
Evseev Anton <soki1997@mail.ru>
2022-08-08 20:45:00 +0500

    Revert "add: fix bags"
    This reverts commit 07aa93832e9ab88a49446093e5451ec5fd0ca37f.

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,6 @@ function App() {
                       <Route path='/setting'> <Profile /></Route>
 
               </div>
-              <div>проверка связи</div>
           </div>
       </BrowserRouter>
   );
