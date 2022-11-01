565a64c7c5dd60a03760a6f7cad9e7e2fdbfbba7
Evseev Anton <soki1997@mail.ru>
2022-11-01 21:01:38 +0500

    add story book add test

---
+++ src/components/Dialogs/Dialogs.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Dialogs from './Dialogs';
+
+
+export default {
+    title: 'Dialogs',
+    component: Dialogs,
+};
+
+export const DialogsTest = () => <Dialogs/>
